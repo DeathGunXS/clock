9a5282084ac1e04f0595a61f76c2c67168c72e80
DeathGunXS <115710052+DeathGunXS@users.noreply.github.com>
2023-03-23 08:40:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+window.onload = function () { //essa linha executa o js imediatamente apos o carregamento de uma pagina.
+
+    var seconds = 00;
+    var tens = 00;
+
+    var appendTens = document.getElementById("tens"); 
+    var appendSeconds = document.getElementById("seconds");
+    
+    var buttonStart = document.getElementById("button-start");
+    var buttonStop = document.getElementById("button-stop");
+    var buttonReset = document.getElementById("button-reset");
+
+    var Interval;
+
+    buttonStart.onclick = function () {
+        clearInterval(Interval) // metodo que impa o temporizador que é definido no setInterval().
+        Interval = setInterval(startTimer, 10) //metodo que chama a função temporizador em intervalos especificados.
+    };
+
+    buttonStop.onclick = function () {
+        clearInterval(Interval)
+    };
+
+    buttonReset.onclick = function () {
+        clearInterval(Interval)
+        tens = "00"
+        seconds = "00"
+
+        appendTens.innerHTML = tens
+        appendSeconds.innerHTML = seconds
+    };
+
+    function startTimer() {
+        tens++;
+
+        if (tens <= 9) {
+            appendTens.innerHTML = "0" + tens;
+        }
+
+        if (tens > 9) {
+            appendTens.innerHTML = tens
+        }
+
+        if (tens > 99) {
+            console.log("seconds");
+            seconds++;
+            appendSeconds.innerHTML = "0" + seconds;
+            tens = 0;
+            appendTens.innerHTML = "0" + 0;
+        }
+
+        if (seconds > 9) {
+            appendSecond.innerHTML = seconds
+        }
+
+        
+    }
+}
